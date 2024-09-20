42275b3e19a6f93498959b2dbeaa54e1e78d9a9c
Roman <dervoedroman05@gmail.com>
2024-09-20 23:35:16 +0300

    Update Task7

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -7,19 +7,14 @@ let passportMarried = {
     }
 };
 
-let newProperty = {married: true};
-let passportMarried2 = {};
-
-for (let key of ['name', 'surname']){
-    passportMarried2[key] = passportMarried[key];
+let passportMarried2 = {
+    name: passportMarried.name,
+    surname: passportMarried.surname,
+    married: true,
+    address: {
+        ...passportMarried.address
+    }
 }
 
-passportMarried2 = {...passportMarried2, ...newProperty};
 
-for (let key of ["address"]){
-    passportMarried2[key] = passportMarried[key];
-}
-
-console.log(passportMarried);
-console.log(passportMarried2);
 
